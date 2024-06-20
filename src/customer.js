836bfe2876703cd5d628bad6a4c5cad2f329aee6
Deval <deval135darji@gmail.com>
2024-06-20 13:33:43 +0530

    Initial Commit
    - Configure basic dependencies
    - Create basic files

---
+++ src/customer.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const PORT = 9000
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.json("Customer service")
+})
+
+app.get('/profile', (req, res) => {
+    return res.json("Customer profile")
+})
+
+app.listen(PORT, () => {
+    console.log(`Customer Server is running on port ${PORT}`);
+})
+
---
+++ src/products.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const PORT = 8000
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.json("Products service")
+})
+
+app.get('/products', (req, res) => {
+    return res.json("Products service")
+})
+
+app.listen(PORT, () => {
+    console.log(`Products Server is running on port ${PORT}`);
+})
+
