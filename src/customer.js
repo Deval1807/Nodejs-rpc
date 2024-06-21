f986995047c029c6688d2571efd25f89789fc31a
Deval <deval135darji@gmail.com>
2024-06-21 09:16:55 +0530

    Update customer.js and products.js
    - Add /wishlist route in customer.js
    - Customer can see his/her wishlist, which we get from product.js through RPC calls
    - Add /customer route in products.js
    - Customer details is visible in product's page, by RPC calls

--- src/customer.js
+++ src/customer.js
@@ -1,20 +1,44 @@
 const express = require('express');
+const { RPCObserver, RPCRequest } = require('./rpc');
 
 const PORT = 9000
 
 const app = express();
 app.use(express.json());
 
+const fakeCustomerResponse  = {
+    _id: "jaskvuih7y83981029ddjfho",
+    name: "Deval",
+    country: "India"
+};
+
+RPCObserver("CUSTOMER_QUEUE", fakeCustomerResponse);
+
 app.get('/', (req, res) => {
     return res.json("Customer service")
 })
 
-app.get('/profile', (req, res) => {
-    return res.json("Customer profile")
+app.get('/wishlist', async (req, res) => { 
+
+    // set the payload
+    const requestPayload = {
+        productId: "001",
+        customerId: "jaskvuih7y83981029ddjfho"
+    };
+
+    try {
+        // send the request to PRODUCTS_QUEUE
+        const responseData = await RPCRequest("PRODUCTS_QUEUE", requestPayload); 
+        console.log("Response Data: ", responseData);
+        return res.status(200).json(responseData)
+    } catch (error) {
+        console.log("Error: ", error);
+        return res.status(500).json(error)
+    }
+
 })
 
 app.listen(PORT, () => {
     console.log(`Customer Server is running on port ${PORT}`);
-    console.clear();
 })
 
--- src/products.js
+++ src/products.js
@@ -1,20 +1,44 @@
 const express = require('express');
+const { RPCObserver, RPCRequest } = require('./rpc');
 
 const PORT = 8000
 
 const app = express();
 app.use(express.json());
 
+
+const fakeProductResponse  = {
+    _id: "001",
+    title: "Apple",
+    price: 50
+};
+
+RPCObserver("PRODUCTS_QUEUE", fakeProductResponse);
+
 app.get('/', (req, res) => {
     return res.json("Products service")
 })
 
-app.get('/products', (req, res) => {
-    return res.json("Products service")
+app.get('/customer', async (req, res) => {
+
+    // set the payload
+    const requestPayload = {
+        customerId: "jaskvuih7y83981029ddjfho"
+    };
+
+    try {
+        // send the request to CUSTOMER_QUEUE
+        const responseData = await RPCRequest("CUSTOMER_QUEUE", requestPayload); 
+        console.log("Response Data: ", responseData);
+        return res.status(200).json(responseData)
+    } catch (error) {
+        console.log("Error: ", error);
+        return res.status(500).json(error)
+    }
+
 })
 
 app.listen(PORT, () => {
     console.log(`Products Server is running on port ${PORT}`);
-    console.clear();
 })
 
